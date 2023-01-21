85d134e69994a4e61abc94115ac04fd34f193df6
Bhinney <snakellie97@gmail.com>
2023-01-21 12:10:55 +0900

    #1 setting : 초기 세팅

---
+++ be/src/main/java/be/GetABeerApplication.java
@@ -0,0 +1,13 @@
+package be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetABeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetABeerApplication.class, args);
+	}
+
+}
---
+++ be/src/test/java/be/GetABeerApplicationTests.java
@@ -0,0 +1,13 @@
+package be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetABeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
