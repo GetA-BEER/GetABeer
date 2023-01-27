1ce91b1a98d2312d62cc65dbeec61080ee0aba56
Bhinney <snakellie97@gmail.com>
2023-01-27 20:22:12 +0900

    GB-23 feat: Implement DTO Class

--- be/src/main/java/be/domain/comment/dto/BeerCommentDto.java
+++ be/src/main/java/be/domain/comment/dto/BeerCommentDto.java
@@ -1,10 +1,53 @@
 package be.domain.comment.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Getter;
+
 public class BeerCommentDto {
 
-	public static class Post {}
+	@Getter
+	@Builder
+	public static class Post {
+		private String nickname;
+		private String content;
+		private Double star;
+	}
 
-	public static class Patch {}
+	@Getter
+	@Builder
+	public static class Patch {
+		private String content;
+		private Double star;
+	}
 
-	public static class Response {}
+	@Getter
+	public static class Response {
+
+		private Long beerCommentId;
+		private String nickname;
+		private String content;
+		private Double star;
+		private Integer likeCount;
+		private Integer recommentCount;
+		private LocalDateTime createdAt;
+		private LocalDateTime modifiedAt;
+
+		protected Response() {
+		}
+
+		@Builder
+		public Response(Long beerCommentId, String nickname, String content, Double star, Integer likeCount,
+			Integer recommentCount, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+			this.beerCommentId = beerCommentId;
+			this.nickname = nickname;
+			this.content = content;
+			this.star = star;
+			this.likeCount = likeCount;
+			this.recommentCount = recommentCount;
+			this.createdAt = createdAt;
+			this.modifiedAt = modifiedAt;
+		}
+	}
 }
--- be/src/main/java/be/domain/pairing/dto/PairingDto.java
+++ be/src/main/java/be/domain/pairing/dto/PairingDto.java
@@ -1,10 +1,72 @@
 package be.domain.pairing.dto;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import be.domain.pairing.entity.PairingImage;
+import lombok.Builder;
+import lombok.Getter;
+
 public class PairingDto {
 
-	public static class Post {}
+	@Getter
+	@Builder
+	public static class Post {
+		private String nickname;
+		private String content;
 
-	public static class Patch {}
+		/*
+		이미지를 어떻게 받을지에 따라 달라질 듯
+		private String imageUrl1;
+		private String imageUrl2;
+		private String imageUrl3;
+		*/
 
-	public static class Response {}
+		private String category;
+	}
+
+	@Getter
+	@Builder
+	public static class Patch {
+
+		private String content;
+
+		/*
+		이미지를 어떻게 받을지에 따라 달라질 듯
+		private String imageUrl1;
+		private String imageUrl2;
+		private String imageUrl3;
+		*/
+
+		private String category;
+	}
+
+	@Getter
+	public static class Response {
+		private Long pairingId;
+		private String nickname;
+		private ArrayList<PairingImage> imageList;
+		private String category;
+		private Integer likeCount;
+		private Integer recommentCount;
+		private LocalDateTime createdAt;
+		private LocalDateTime modifiedAt;
+
+		protected Response() {
+		}
+
+		@Builder
+		public Response(Long pairingId, String nickname, ArrayList<PairingImage> imageList, String category,
+			Integer likeCount,
+			Integer recommentCount, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+			this.pairingId = pairingId;
+			this.nickname = nickname;
+			this.imageList = imageList;
+			this.category = category;
+			this.likeCount = likeCount;
+			this.recommentCount = recommentCount;
+			this.createdAt = createdAt;
+			this.modifiedAt = modifiedAt;
+		}
+	}
 }
