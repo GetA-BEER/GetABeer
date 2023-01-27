5864db4aa9710afadc851d5e445b376a84355791
Bhinney <snakellie97@gmail.com>
2023-01-27 21:45:54 +0900

    GB-23 add: Add ExceptionCode

--- be/src/main/java/be/global/exception/ExceptionCode.java
+++ be/src/main/java/be/global/exception/ExceptionCode.java
@@ -4,20 +4,24 @@ import lombok.Getter;
 
 public enum ExceptionCode {
 
+	/* Comment 관련 예외 */
+	BEER_COMMENT_NOT_FOUND(404, "맥주 코멘트 정보를 찾을 수 없습니다."),
+	PAIRING_NOT_FOUND(404, "페어링 정보를 찾을 수 없습니다."),
 
-    USER_NOT_FOUND(404,"User Not Found"),
-    USER_ID_EXISTS(409, "User ID Exists"),
-    NICKNAME_EXISTS(409, "Nickname Exists"),
-    UNAUTHORIZED(401, "Unauthorized"), // 인증이 필요한 상태
-    FORBIDDEN(403, "Forbidden"); // 인증은 되었으나 권한이 없는 상태
+	/* USER 관련 예외 */
+	USER_NOT_FOUND(404, "User Not Found"),
+	USER_ID_EXISTS( 409, "User ID Exists"),
+	NICKNAME_EXISTS(409, "Nickname Exists"),
+	UNAUTHORIZED(401, "Unauthorized"), // 인증이 필요한 상태
+	FORBIDDEN(403, "Forbidden"); // 인증은 되었으나 권한이 없는 상태
 
-    @Getter
-    private int status;
-    @Getter
-    private String message;
+	@Getter
+	private int status;
+	@Getter
+	private String message;
 
-    ExceptionCode(int status, String message) {
-        this.status = status;
-        this.message = message;
-    }
+	ExceptionCode(int status, String message) {
+		this.status = status;
+		this.message = message;
+	}
 }
