2b00970325aea7a2b68c12f40a53ef976e373cff
hyojin <hyoreal51@gmail.com>
2023-01-31 17:11:33 +0900

    GB-28 feat: add JwtAuthenticationFilter, user role / fix: age, gender change the type from String to enum

--- be/src/main/java/be/domain/user/entity/User.java
+++ be/src/main/java/be/domain/user/entity/User.java
@@ -8,10 +8,13 @@ import be.domain.pairing.entity.Pairing;
 import be.domain.recomment.entity.BeerRecomment;
 import be.domain.recomment.entity.PairingRecomment;
 import be.domain.user.dto.UserDto;
+import be.domain.user.entity.enums.Age;
+import be.domain.user.entity.enums.Gender;
 import be.global.BaseTimeEntity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -40,10 +43,13 @@ public class User extends BaseTimeEntity {
     private String imageUrl;
 
     @Column
-    private String gender;
+    private String provider;
 
-    @Column
-    private int age;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private Age age;
 
     public User(String email, String nickname, String password) {
         this.email = email;
@@ -54,6 +60,9 @@ public class User extends BaseTimeEntity {
     public void edit(String nickname) {
         this.nickname = nickname;
     }
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
 
     /* UserBeerTag Join */
     @OneToMany(mappedBy = "user")
---
+++ be/src/main/java/be/domain/user/entity/enums/Gender.java
@@ -0,0 +1,7 @@
+package be.domain.user.entity.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    REFUSE;
+}
