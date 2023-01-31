c72e031219647cda482bc07705204b8f69b6d717
Bhinney <snakellie97@gmail.com>
2023-01-31 19:07:23 +0900

    GB-27 refactor: BeerComment -> Rating

--- be/src/main/java/be/domain/beer/repository/BeerQueryRepository.java
+++ be/src/main/java/be/domain/beer/repository/BeerQueryRepository.java
@@ -1,14 +1,19 @@
 package be.domain.beer.repository;
 
 import be.domain.beer.entity.Beer;
+
 import static be.domain.beer.entity.QBeer.beer;
 import static be.domain.beer.entity.QBeerBeerTag.beerBeerTag;
 
 import be.domain.beertag.entity.BeerTag;
+
 import static be.domain.beertag.entity.QBeerTag.beerTag;
-import static be.domain.rating.entity.QBeerComment.beerComment;
+import static be.domain.rating.entity.QRating.*;
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -17,27 +22,27 @@ import java.util.List;
 @Repository
 @RequiredArgsConstructor
 public class BeerQueryRepository {
-    private final JPAQueryFactory jpaQueryFactory;
+	private final JPAQueryFactory jpaQueryFactory;
 
-    public List<Beer> findMonthlyBeer() {
-        return jpaQueryFactory.selectFrom(beer)
-                .join(beer.beerCommentList, beerComment)
-                .where(beerComment.createdAt.month().eq(LocalDateTime.now().getMonthValue() - 1))
-                .orderBy(beer.beerDetailsStars.totalAverageStars.desc())
-                .limit(5)
-                .fetch();
-    }
+	public List<Beer> findMonthlyBeer() {
+		return jpaQueryFactory.selectFrom(beer)
+			.join(beer.ratingList, rating)
+			.where(rating.createdAt.month().eq(LocalDateTime.now().getMonthValue() - 1))
+			.orderBy(beer.beerDetailsStars.totalAverageStars.desc())
+			.limit(5)
+			.fetch();
+	}
 
-    public List<BeerTag> findTop4BeerTag(Beer findBeer) {
+	public List<BeerTag> findTop4BeerTag(Beer findBeer) {
 
-        return jpaQueryFactory
-                .selectFrom(beerTag)
-                .join(beerTag.beerBeerTags, beerBeerTag)
-                .where(beerBeerTag.beer.eq(findBeer))
-                .groupBy(beerTag)
-                .orderBy(beerTag.count().desc())
-                .limit(4)
-                .fetch();
+		return jpaQueryFactory
+			.selectFrom(beerTag)
+			.join(beerTag.beerBeerTags, beerBeerTag)
+			.where(beerBeerTag.beer.eq(findBeer))
+			.groupBy(beerTag)
+			.orderBy(beerTag.count().desc())
+			.limit(4)
+			.fetch();
 
-    }
+	}
 }
