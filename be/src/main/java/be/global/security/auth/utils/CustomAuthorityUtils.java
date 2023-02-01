1fb449cbae0d8727740ec3f0e41b3b01c71bd7b7
hyojin <hyoreal51@gmail.com>
2023-02-01 14:50:36 +0900

    GB-28 feat: add CustomAuthorityUtils

--- be/src/main/java/be/global/security/auth/utils/CustomAuthorityUtils.java
+++ be/src/main/java/be/global/security/auth/utils/CustomAuthorityUtils.java
@@ -1,10 +1,12 @@
 package be.global.security.auth.utils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,4 +20,23 @@ public class CustomAuthorityUtils {
 	private final List<String> ADMIN_ROLES_STRING = List.of("ADMIN", "USER");
 	private final List<String> USER_ROLES_STRING = List.of("USER");
 
+	public List<GrantedAuthority> createAuthorities(String email) {
+		if (email.equals(adminMailAddress)) {
+			return ADMIN_ROLES;
+		}
+		return USER_ROLES;
+	}
+
+	public List<GrantedAuthority> createAuthorities(List<String> roles) {
+		return roles.stream()
+			.map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+			.collect(Collectors.toList());
+	}
+
+	public List<String> createRoles(String email) {
+		if (email.equals(adminMailAddress)) {
+			return ADMIN_ROLES_STRING;
+		}
+		return USER_ROLES_STRING;
+	}
 }
