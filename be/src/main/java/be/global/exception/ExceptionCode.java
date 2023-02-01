4cfc10b1c8e1531c52ec1204d1a2592112364476
Bhinney <snakellie97@gmail.com>
2023-02-01 23:15:09 +0900

    GB-27 add: Add ExceptionCode

--- be/src/main/java/be/global/exception/ExceptionCode.java
+++ be/src/main/java/be/global/exception/ExceptionCode.java
@@ -8,6 +8,7 @@ public enum ExceptionCode {
 	RATING_NOT_FOUND(404, "맥주 평가 정보를 찾을 수 없습니다."),
 	PAIRING_NOT_FOUND(404, "페어링 정보를 찾을 수 없습니다."),
 	TAG_IS_WRONG(400, "태그 요청이 잘못 되었습니다."),
+	IMAGE_SIZE_OVER(400, "이미지는 세 장까지만 등록이 가능합니다."),
 
 	/* USER 관련 예외 */
 	USER_NOT_FOUND(404, "User Not Found"),
