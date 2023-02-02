cdf348e1232ad22d07df8c9fd6618a7afd8bbbed
Eugenius1st <eugenius1st@gmail.com>
2023-02-02 13:29:42 +0900

    GB-37 fix: 오타수정

--- fe/pages/beer/index.tsx
+++ fe/pages/beer/index.tsx
@@ -100,7 +100,7 @@ export default function Beer() {
     },
   ];
   let ratingCount = 35;
-  let paringCount = 3;
+  let pairngCount = 3;
 
   return (
     <>
@@ -130,7 +130,7 @@ export default function Beer() {
         <RatingTitle ratingCount={ratingCount} />
         <SmallCardController cardProps={cardProps} />
 
-        <PairingTitle paringCount={paringCount} />
+        <PairingTitle pairngCount={pairngCount} />
         <SmallPairingController pairProps={pairProps} />
 
         <SimilarBeerController beerProps={beerProps} />
