b2a8c110efb7b781b1567a2a09b1450b62c960ec
serin-B <bangserin980302@gmail.com>
2023-02-06 09:33:56 +0900

    GB-51 style: header position: sticky로 고정

--- fe/components/Header.tsx
+++ fe/components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
     if (inputRef.current !== null) inputRef.current.focus();
   });
   return (
-    <div className="flex justify-between items-center h-20 border-b border-y-lightGray">
+    <div className="flex justify-between items-center h-20 border-b border-y-lightGray bg-white sticky top-0 z-10">
       <Link href={'/'}>
         <Image
           alt="logo"
