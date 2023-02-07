b26f5fdb6438ec5fbe13e69494023decacf53471
hyojin <hyoreal51@gmail.com>
2023-02-07 11:05:28 +0900

    GB-59 feat/fix : add AuthUser, fix UserDeatailService, PasswordEncoder NPE 해결

--- be/src/main/java/be/global/security/auth/userdetails/UserDetailService.java
+++ be/src/main/java/be/global/security/auth/userdetails/UserDetailService.java
@@ -1,8 +1,7 @@
 package be.global.security.auth.userdetails;
 
-import java.util.Collection;
-
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,7 +11,6 @@ import be.domain.user.entity.User;
 import be.domain.user.repository.UserRepository;
 import be.global.exception.BusinessLogicException;
 import be.global.exception.ExceptionCode;
-import be.global.security.auth.utils.CustomAuthorityUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,57 +20,16 @@ import lombok.extern.slf4j.Slf4j;
 public class UserDetailService implements UserDetailsService {
 
 	private final UserRepository userRepository;
-	private final CustomAuthorityUtils customAuthorityUtils;
 
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		User user = userRepository.findByEmail(email)
+		return userRepository.findByEmail(email)
+			.map(this::createUserDetails)
 			.orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
-
-		return new UserDetailsImpl(user);
 	}
 
-	public class UserDetailsImpl extends User implements UserDetails {
-		UserDetailsImpl(User user) {
-			User.builder()
-				.id(user.getId())
-				// .age(user.getAge())
-				.roles(user.getRoles())
-				.email(user.getEmail())
-				// .gender(user.getGender())
-				.nickname(user.getNickname())
-				.password(user.getPassword())
-				.build();
-		}
-
-		@Override
-		public Collection<? extends GrantedAuthority> getAuthorities() {
-			return customAuthorityUtils.createAuthorities(this.getRoles());
-		}
-
-		@Override
-		public String getUsername() {
-			return getEmail();
-		}
-
-		@Override
-		public boolean isAccountNonExpired() {
-			return true;
-		}
-
-		@Override
-		public boolean isAccountNonLocked() {
-			return true;
-		}
-
-		@Override
-		public boolean isCredentialsNonExpired() {
-			return true;
-		}
-
-		@Override
-		public boolean isEnabled() {
-			return true;
-		}
+	private UserDetails createUserDetails(User user) {
+		GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(user.getRoles().get(0));
+		return AuthUser.of(user);
 	}
 }
