b6910cb56fb524a3a4b1a201c5f50a1f2a84c5e3
serin-B <bangserin980302@gmail.com>
2023-02-08 05:22:00 +0900

    feat: reqBody에 필요한 useState 생성

--- fe/pages/postrating/index.tsx
+++ fe/pages/postrating/index.tsx
@@ -2,26 +2,45 @@ import StarRatingComponent from 'react-rating-stars-component';
 import { useState } from 'react';
 import ColorTag from '@/components/tag/ColorTag';
 import SmellTag from '@/components/tag/SmellTag';
-import SparkleTag from '@/components/tag/sparkleTag';
+import SparkleTag from '@/components/tag/SparkleTag';
 import TasteTag from '@/components/tag/TasteTag';
 import BigInput from '@/components/inputs/BigInput';
 import CloseBtn from '@/components/button/CloseBtn';
 import SubmitBtn from '@/components/button/SubmitBtn';
+import MiddleCard, { testBeer } from '@/components/middleCards/MiddleCard';
 
 export default function PostRatingPage() {
   const [star, setStar] = useState(0);
-  const [tagList, setTagList] = useState([]);
+  const [content, setContent] = useState('');
+  const [color, setColor] = useState('');
+  const [flavor, setFlavor] = useState('');
+  const [taste, setTaste] = useState('');
+  const [carbonation, setCarbonation] = useState('');
+
   const ratingChanged = (newRating: number) => {
     setStar(newRating);
   };
+
+  const reqBody = {
+    star,
+    content,
+    color,
+    flavor,
+    taste,
+    carbonation,
+  };
+
+  console.log(reqBody);
+
   return (
     <div className="m-auto h-screen max-w-4xl px-6">
-      <form>
+      <MiddleCard cardProps={testBeer} />
+      <div>
         <div>별점</div>
         <div className="flex justify-start items-center">
           <StarRatingComponent
             count={5}
-            size={40}
+            size={50}
             value={star}
             onChange={ratingChanged}
             isHalf={true}
@@ -31,12 +50,16 @@ export default function PostRatingPage() {
           <span className="text-2xl ml-6">{star}</span>
         </div>
         <div>평가</div>
-        <ColorTag />
-        <SmellTag />
-        <SparkleTag />
-        <TasteTag />
+        <ColorTag setSelected={setColor} />
+        <SmellTag setSelected={setFlavor} />
+        <TasteTag setSelected={setTaste} />
+        <SparkleTag setSelected={setCarbonation} />
         <div>리뷰</div>
-        <BigInput placeholder="맥주에 대한 평가를 남겨주세요" />
+        <BigInput
+          placeholder="맥주에 대한 평가를 남겨주세요"
+          inputState={content}
+          setInputState={setContent}
+        />
         <div className="flex">
           <div className="flex-1">
             <CloseBtn
@@ -57,7 +80,7 @@ export default function PostRatingPage() {
             </SubmitBtn>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
