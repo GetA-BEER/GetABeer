4b9ab0c1dd9fc80280cd6c1150f6c5456af88438
SeongYum <dbal6930@gmail.com>
2023-02-07 20:26:15 +0900

    GB-57 style:마이 페이지, 회원정보 수정 페이지 제작

--- fe/pages/mypage/index.tsx
+++ fe/pages/mypage/index.tsx
@@ -25,56 +25,58 @@ export default function Mypage() {
           마이 페이지
         </div>
         <div className="flex flex-col items-center my-6">
-          <div className="w-28 h-28 bg-y-cream rounded-xl"></div>
+          <div className="w-24 h-24 bg-y-cream rounded-2xl"></div>
           <div className="flex justify-center items-center gap-1 mt-2">
-            <div>성유미님</div>
+            <div className="text-sm">성유미님</div>
             <button className="w-5 h-5 text-y-brown">
               <HiPencil className="w-5 h-5" />
             </button>
           </div>
         </div>
-        <div className="m-2 border divide-y divide-gray-200 rounded-xl text-sm">
-          <button className="flex w-full p-5 rounded-t-xl justify-between hover:bg-gray-200 ">
-            <div className="flex gap-2">
-              <IoHeartOutline className="self-center w-5 h-5" />
-              위시 페이지
-            </div>
-            <IoChevronForwardOutline className="w-5 h-5 " />
-          </button>
-          <button className="flex w-full p-5 justify-between hover:bg-gray-200 ">
-            <div className="flex gap-2 ">
-              <HiOutlinePencil className="self-center w-5 h-5" />
-              나의 코멘트
-            </div>
-            <IoChevronForwardOutline className="w-5 h-5" />
-          </button>
-          <button className="flex w-full p-5 justify-between hover:bg-gray-200 ">
-            <div className="flex gap-2 ">
-              <HiOutlineChartPie className="self-center w-5 h-5" />
-              나의 페어링
-            </div>
-            <IoChevronForwardOutline className="w-5 h-5" />
-          </button>
-          <button className="flex w-full p-5 justify-between hover:bg-gray-200">
-            <div className="flex gap-2 ">
-              <IoChatboxEllipsesOutline className="self-center w-5 h-5" />
-              나의 댓글
-            </div>
-            <IoChevronForwardOutline className="w-5 h-5" />
-          </button>
-          <button className="flex w-full p-5 justify-between hover:bg-gray-200">
-            <div className="flex gap-2 ">
-              <HiOutlineMapPin className="self-center w-5 h-5" />
-              관심 가게
-            </div>
-            <IoChevronForwardOutline className="w-5 h-5" />
-          </button>
-          <button className="flex w-full p-5 rounded-b-xl justify-between hover:bg-gray-200">
-            <div className="flex gap-2 ">
-              <IoLogOutOutline className="self-center w-5 h-5" />
-              로그아웃
-            </div>
-          </button>
+        <div className="m-auto max-w-md px-3 pb-5">
+          <div className="border divide-y divide-gray-200 rounded-xl text-sm">
+            <button className="flex w-full p-5 rounded-t-xl justify-between hover:bg-gray-200 ">
+              <div className="flex gap-2">
+                <IoHeartOutline className="self-center w-5 h-5" />
+                위시 페이지
+              </div>
+              <IoChevronForwardOutline className="w-5 h-5 " />
+            </button>
+            <button className="flex w-full p-5 justify-between hover:bg-gray-200 ">
+              <div className="flex gap-2 ">
+                <HiOutlinePencil className="self-center w-5 h-5" />
+                나의 코멘트
+              </div>
+              <IoChevronForwardOutline className="w-5 h-5" />
+            </button>
+            <button className="flex w-full p-5 justify-between hover:bg-gray-200 ">
+              <div className="flex gap-2 ">
+                <HiOutlineChartPie className="self-center w-5 h-5" />
+                나의 페어링
+              </div>
+              <IoChevronForwardOutline className="w-5 h-5" />
+            </button>
+            <button className="flex w-full p-5 justify-between hover:bg-gray-200">
+              <div className="flex gap-2 ">
+                <IoChatboxEllipsesOutline className="self-center w-5 h-5" />
+                나의 댓글
+              </div>
+              <IoChevronForwardOutline className="w-5 h-5" />
+            </button>
+            <button className="flex w-full p-5 justify-between hover:bg-gray-200">
+              <div className="flex gap-2 ">
+                <HiOutlineMapPin className="self-center w-5 h-5" />
+                관심 가게
+              </div>
+              <IoChevronForwardOutline className="w-5 h-5" />
+            </button>
+            <button className="flex w-full p-5 rounded-b-xl justify-between hover:bg-gray-200">
+              <div className="flex gap-2 ">
+                <IoLogOutOutline className="self-center w-5 h-5" />
+                로그아웃
+              </div>
+            </button>
+          </div>
         </div>
       </main>
     </>
