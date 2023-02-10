7f4b3cc2d5eafb293719fee35eaad0830b7936b1
Eugenius 1st <eugenius1st@gmail.com>
2023-02-11 00:47:41 +0900

    GB-56 feat/main: 메인페이지 카메라 및 이미지 기능 연결

--- fe/components/modals/CameraModal.tsx
+++ fe/components/modals/CameraModal.tsx
@@ -4,10 +4,26 @@ import { BsImages } from 'react-icons/bs';
 import { AiFillCamera, AiOutlineClose } from 'react-icons/ai';
 
 export default function CameraModal() {
+  interface FileData {
+    lastModified: number;
+    name: string;
+    size: number;
+    type: string;
+  }
+
   const [showModal, setShowModal] = useState(false);
+  const [uploadImg, setUploadImg] = useState<FileData | null>(null);
+
   const onCameraClick = () => {
     setShowModal(true);
-    console.log('보여야 하는데', showModal);
+  };
+
+  const imageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files !== null) {
+      const imageFile = e.target.files[0];
+      setUploadImg(imageFile);
+      console.log(imageFile);
+    }
   };
 
   return (
@@ -17,46 +33,46 @@ export default function CameraModal() {
         <div className="text-[8px]">이미지 검색</div>
       </div>
       {showModal ? (
-        <div className="fixed grid grid-cols-3 gap-2 bottom-16 left-1/2 -ml-24 text-y-brown">
-          <div
-            onClick={() => setShowModal(false)}
-            className="bg-white p-1 rounded-full w-14 h-14 border-2 border-y-brown flex justify-center items-center"
-          >
-            <AiOutlineClose className="w-5 h-5" />
-          </div>
-          {/* <form> */}
-          <div className="bg-white p-1 rounded-full w-14 h-14 border-2 border-y-brown -mt-8">
-            <label htmlFor="file">
-              <BsImages className="w-5 h-5 block m-auto mt-[6px] -mb-1" />
-              <span className="text-[8px] w-fit m-auto">갤러리</span>
-            </label>
-            <input type="file" id="file" accept="image/*" className="hidden" />
-          </div>
+        <form className="fixed bottom-16 left-1/2 -ml-16 animate-upper">
+          <div className="grid grid-cols-2 gap-2  text-y-brown ">
+            <div className="bg-white p-1 rounded-full w-14 h-14 border-2 border-y-brown">
+              <label htmlFor="camera">
+                <AiFillCamera className="w-6 h-6 block m-auto mt-1 -mb-1" />
+                <span className="text-[8px] w-fit m-auto">촬영</span>
+              </label>
+              <input
+                type="file"
+                id="camera"
+                accept="image/*"
+                capture="environment"
+                className="hidden"
+                onChange={imageUpload}
+              />
+            </div>
 
-          <div className="bg-white p-1 rounded-full w-14 h-14 border-2 border-y-brown">
-            <label htmlFor="camera">
-              <AiFillCamera className="w-6 h-6 block m-auto mt-1 -mb-1" />
-              <span className="text-[8px] w-fit m-auto">촬영</span>
-            </label>
-            <input
-              type="file"
-              id="camera"
-              accept="image/*"
-              capture="environment"
-              className="hidden"
-            />
+            <div className="bg-white p-1 rounded-full w-14 h-14 border-2 border-y-brown">
+              <label htmlFor="file">
+                <BsImages className="w-5 h-5 block m-auto mt-[6px] -mb-1" />
+                <span className="text-[8px] w-fit m-auto">갤러리</span>
+              </label>
+              <input
+                type="file"
+                id="file"
+                accept="image/*"
+                className="hidden"
+                onChange={imageUpload}
+              />
+            </div>
+            <button
+              type="button"
+              className="inset-0 fixed cursor-default -z-10"
+              onClick={() => setShowModal(false)}
+            ></button>
           </div>
-          {/* </form> */}
-        </div>
+        </form>
       ) : (
         <></>
       )}{' '}
     </>
   );
-}
-{
-  /* <button
-className="inset-0 fixed cursor-default"
-onClick={() => setShowModal(false)}
-></button> */
 }
