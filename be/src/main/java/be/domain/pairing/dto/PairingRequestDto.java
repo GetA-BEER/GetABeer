f46d303567e1eca77f151814ef91937915b8f788
Bhinney <snakellie97@gmail.com>
2023-02-15 01:21:20 +0900

    GB-60 add: 유효성 검증 추가

--- be/src/main/java/be/domain/pairing/dto/PairingRequestDto.java
+++ be/src/main/java/be/domain/pairing/dto/PairingRequestDto.java
@@ -2,6 +2,8 @@ package be.domain.pairing.dto;
 
 import java.util.List;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,21 +12,33 @@ public class PairingRequestDto {
 	@Getter
 	@Builder
 	public static class Post {
+
+		@NotBlank
 		private Long beerId;
+
+		@NotBlank
 		private Long userId;
+
+		@NotBlank
 		private String content;
-		// private List<MultipartFile> files;
+
+		@NotBlank
 		private String category;
 	}
 
 	@Getter
 	@Builder
 	public static class Patch {
+
+		@NotBlank
 		private Long beerId;
 		private String content;
+
+		@NotBlank
 		private List<String> type;
 		private List<Long> url;
-		// private List<MultipartFile> newFile;
+
+		@NotBlank
 		private String category;
 	}
 }
--- be/src/main/java/be/domain/rating/dto/RatingRequestDto.java
+++ be/src/main/java/be/domain/rating/dto/RatingRequestDto.java
@@ -1,5 +1,7 @@
 package be.domain.rating.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,25 +10,49 @@ public class RatingRequestDto {
 	@Getter
 	@Builder
 	public static class Post {
+
+		@NotBlank
 		private Long beerId;
+
+		@NotBlank
 		private Long userId;
 		private String content;
+
+		@NotBlank
 		private Double star;
+
+		@NotBlank
 		private String color;
+
+		@NotBlank
 		private String taste;
+
+		@NotBlank
 		private String flavor;
+
+		@NotBlank
 		private String carbonation;
 	}
 
 	@Getter
 	@Builder
 	public static class Patch {
+
+		@NotBlank
 		private Long beerId;
 		private String content;
 		private Double star;
+
+		@NotBlank
 		private String color;
+
+		@NotBlank
 		private String taste;
+
+		@NotBlank
 		private String flavor;
+
+		@NotBlank
 		private String carbonation;
 	}
 }
