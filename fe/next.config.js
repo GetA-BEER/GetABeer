65cc1fd3162a773d1c08f8fa5d177d987b8b538d
serin-B <bangserin980302@gmail.com>
2023-02-19 13:42:24 +0900

    feat: axios 인스턴스 및 env 생성

--- fe/next.config.js
+++ fe/next.config.js
@@ -4,13 +4,8 @@ const nextConfig = {
     domains: ['worldbeermarket.kr', 't1.daumcdn.net', 'assabeer.com'],
   },
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:slug*',
-        destination: 'http://localhost:8080/api/:slug*',
-      },
-    ];
+  env: {
+    API_URL: process.env.API_URL,
   },
 };
 
