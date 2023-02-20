8067baa6541f084db26363e2f11d8926b9502381
serin-B <bangserin980302@gmail.com>
2023-02-20 23:51:27 +0900

    chore: 버셀 배포 테스트6

--- fe/pages/postrating/index.tsx
+++ fe/pages/postrating/index.tsx
@@ -54,7 +54,7 @@ export default function PostRatingPage() {
   const handleSubmit = () => {
     const reqBody = {
       beerId: 1,
-      userId: 5,
+      userId: 6,
       star,
       content,
       color: TagMatcherToEng(color),
