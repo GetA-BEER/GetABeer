652648d06a818fe647c915a83b794d450e871725
serin-B <bangserin980302@gmail.com>
2023-02-20 23:28:06 +0900

    chore: 버셀 테스트

--- fe/pages/postrating/index.tsx
+++ fe/pages/postrating/index.tsx
@@ -54,7 +54,7 @@ export default function PostRatingPage() {
   const handleSubmit = () => {
     const reqBody = {
       beerId: 1,
-      userId: 10,
+      userId: 1,
       star,
       content,
       color: TagMatcherToEng(color),
