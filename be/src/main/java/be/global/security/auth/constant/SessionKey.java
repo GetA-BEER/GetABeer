365439be7bb87d8bc95e60ee546b69ad1db46a56
hyojin <hyoreal51@gmail.com>
2023-02-21 22:01:06 +0900

    GB-83 feat/fix: oauth2 레디스 세션 도입

---
+++ be/src/main/java/be/global/security/auth/constant/SessionKey.java
@@ -0,0 +1,5 @@
+package be.global.security.auth.constant;
+
+public class SessionKey {
+	public static final String LOGIN_USER_ID = "USER_ID";
+}
---
+++ be/src/main/java/be/global/security/auth/oauth/strategy/userinfo/GoogleUserInfo.java
@@ -0,0 +1,42 @@
+package be.global.security.auth.oauth.strategy.userinfo;
+
+import java.io.Serializable;
+import java.util.Map;
+
+public class GoogleUserInfo implements OAuth2UserInfo, Serializable {
+	private Map<String, Object> attributes;
+
+	public GoogleUserInfo(Map<String, Object> attributes) {
+		this.attributes = attributes;
+	}
+
+	@Override
+	public Map<String, Object> getAttributes() {
+		return this.attributes;
+	}
+
+	@Override
+	public String getProviderId() {
+		return attributes.get("sub").toString();
+	}
+
+	@Override
+	public String getProvider() {
+		return "google";
+	}
+
+	@Override
+	public String getImageUrl() {
+		return attributes.get("picture").toString();
+	}
+
+	@Override
+	public String getEmail() {
+		return attributes.get("email").toString();
+	}
+
+	@Override
+	public String getName() {
+		return attributes.get("name").toString();
+	}
+}
