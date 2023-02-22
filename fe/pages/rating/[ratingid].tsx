b23ebb58448536844c82613f04fa348372f2fb9d
serin-B <bangserin980302@gmail.com>
2023-02-22 13:16:24 +0900

    GB-67 feat: 상세페이지 댓글기능 추가

--- fe/pages/rating/[ratingid].tsx
+++ fe/pages/rating/[ratingid].tsx
@@ -7,32 +7,64 @@ import RatingCard, {
 } from '@/components/middleCards/RatingCard';
 import CommentInput from '@/components/inputs/CommentInput';
 import PageContainer from '@/components/PageContainer';
+import SpeechBalloon from '@/components/SpeechBalloon';
+import { RatingComment } from '@/components/SpeechBalloon';
 
 export default function Rating() {
   const router = useRouter();
   const ratingId = router.query.ratingid;
+
   const [cardProps, setCardProps] = useState<RatingCardProps>();
   const [inputState, setInputState] = useState<string>('');
+  const [ratingCommentList, setRatingCommentList] = useState<
+    RatingComment[] | null
+  >(null);
+
   useEffect(() => {
     if (ratingId !== undefined) {
-      axios.get(`/ratings/${ratingId}`).then((res) => {
-        setCardProps(res.data);
-      });
+      axios
+        .get(`/ratings/${ratingId}`)
+        .then((res) => {
+          setCardProps(res.data);
+          setRatingCommentList(res.data.ratingCommentList);
+        })
+        .catch((err) => console.log(err));
     }
   }, [ratingId]);
 
-  const postComment = () => {
-    const reqBody = {
-      userId: 1,
-      ratingId: Number(ratingId),
-      content: inputState,
-    };
-    console.log(reqBody);
-    setInputState('');
-    // axios.post('/ratings/comments', reqBody).then((res) => {
-    //   console.log(res);
-    //   setInputState('');
-    // });
+  const postRatingComment = () => {
+    if (inputState !== '') {
+      const reqBody = {
+        userId: 1,
+        ratingId: Number(ratingId),
+        content: inputState,
+      };
+      axios
+        .post('/ratings/comments', reqBody)
+        .then((res) => {
+          if (ratingCommentList === null) {
+            setRatingCommentList([res.data]);
+          } else {
+            setRatingCommentList([res.data, ...ratingCommentList]);
+          }
+          setInputState('');
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+  const deleteRatingComment = (ratingCommentId: number) => {
+    axios
+      .delete(`/ratings/comments/${ratingCommentId}`)
+      .then((res) => {
+        if (ratingCommentList !== null) {
+          const filtered = ratingCommentList.filter((el) => {
+            return el.ratingCommentId !== ratingCommentId;
+          });
+          setRatingCommentList(filtered);
+        }
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -49,14 +81,32 @@ export default function Rating() {
         </div>
         <div className="border border-y-lightGray rounded-lg px-3 py-4 m-2">
           {cardProps !== undefined ? (
-            <RatingCard cardProps={cardProps} isMine={true} />
+            <RatingCard
+              cardProps={cardProps}
+              isMine={true}
+              count={ratingCommentList ? ratingCommentList?.length : 0}
+            />
           ) : null}
           <div className="my-5">
             <CommentInput
               inputState={inputState}
               setInputState={setInputState}
-              postFunc={postComment}
+              postFunc={postRatingComment}
             />
+          </div>
+          <div>
+            {ratingCommentList === null
+              ? null
+              : ratingCommentList.map((el) => {
+                  return (
+                    <SpeechBalloon
+                      key={el.ratingCommentId}
+                      props={el}
+                      isMine={true}
+                      deleteFunc={deleteRatingComment}
+                    />
+                  );
+                })}
           </div>
         </div>
       </div>
