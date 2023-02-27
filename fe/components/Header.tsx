83560d00b49405ce650bb22945fef5d63f6e1de4
Eugenius 1st <eugenius1st@gmail.com>
2023-02-27 17:59:51 +0900

    GB-87 style: 헤더 크기 조절

--- fe/components/Header.tsx
+++ fe/components/Header.tsx
@@ -30,25 +30,27 @@ export default function Header() {
     }
   }, [TOKEN]);
   return (
-    <div className="flex justify-between items-center max-w-4xl m-auto h-16 border-b border-y-lightGray bg-white sticky top-0 z-10">
-      <Link href={'/'}>
-        <Image
-          alt="logo"
-          width={50}
-          height={50}
-          src="/images/logo.png"
-          className="ml-4 mr-2"
-        />
-      </Link>
-      {isSearching ? <SearchModal setIsSearching={setIsSearching} /> : null}
-      <button
-        className="mr-4"
-        onClick={() => {
-          setIsSearching(true);
-        }}
-      >
-        <BiSearch className="w-[30px] h-[30px]" />
-      </button>
+    <div className="w-full bg-white sticky top-0 z-10 border-b mb-6">
+      <div className="flex justify-between items-center max-w-4xl m-auto h-16 border-y-lightGray ">
+        <Link href={'/'}>
+          <Image
+            alt="logo"
+            width={48}
+            height={48}
+            src="/images/logo.png"
+            className="ml-4 mr-2"
+          />
+        </Link>
+        {isSearching ? <SearchModal setIsSearching={setIsSearching} /> : null}
+        <button
+          className="mr-4"
+          onClick={() => {
+            setIsSearching(true);
+          }}
+        >
+          <BiSearch className="w-[30px] h-[30px]" />
+        </button>
+      </div>
     </div>
   );
 }
--- fe/components/NavBar.tsx
+++ fe/components/NavBar.tsx
@@ -17,8 +17,8 @@ export default function NavBar() {
     }
   }, [TOKEN]);
   return (
-    <nav className="w-full m-auto fixed bottom-0 z-[9] border-gray-200 select-none">
-      <div className="max-w-4xl m-auto border-t grid grid-cols-5 px-5 bg-white text-center py-1">
+    <nav className="w-full m-auto fixed bottom-0 z-[9] border-t border-gray-200 select-none">
+      <div className="max-w-4xl m-auto  grid grid-cols-5 px-5 bg-white text-center py-1">
         <Link href={'/'} className="hover:text-y-brown">
           <AiOutlineHome className="m-auto text-3xl py-[1px]" />
           <div className="text-[8px]">홈</div>
