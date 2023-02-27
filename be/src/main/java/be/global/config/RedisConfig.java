87434d6811cce755dd801898559dad8d90aee189
Bhinney <snakellie97@gmail.com>
2023-02-27 14:09:42 +0900

    GB-60 fix: 필요없는 임포트 제거

--- be/src/main/java/be/global/config/RedisConfig.java
+++ be/src/main/java/be/global/config/RedisConfig.java
@@ -21,7 +21,6 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.session.data.redis.config.ConfigureRedisAction;
 
 @EnableCaching
 @Configuration
