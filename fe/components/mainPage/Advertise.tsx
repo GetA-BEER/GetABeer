54cf03a0f15506b8962cf139e3dd4fd739d5752c
Eugenius 1st <eugenius1st@gmail.com>
2023-02-28 18:49:19 +0900

    GB-98 chore: 맥주 광고 수정

--- fe/components/mainPage/Advertise.tsx
+++ fe/components/mainPage/Advertise.tsx
@@ -5,6 +5,13 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Pagination, Navigation, Autoplay } from 'swiper';
 export default function Advertise() {
+  const imageList = [
+    `/images/advertise/main.png`,
+    `/images/advertise/search.png`,
+    `/images/advertise/rating.png`,
+    `/images/advertise/pairing.png`,
+    `/images/advertise/map.png`,
+  ];
   return (
     <Swiper
       pagination={true}
@@ -14,66 +21,18 @@ export default function Advertise() {
       autoplay={{ delay: 2000 }}
       className="mySwiper -mt-6"
     >
-      <SwiperSlide className="w-full">
-        <Image
-          className="w-full bg-y-gold"
-          src={`/images/advertise/main.png`}
-          alt="adv1"
-          width={500}
-          height={500}
-          priority
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          className="w-full"
-          src={`/images/advertise/main2.png`}
-          alt="adv2"
-          width={500}
-          height={500}
-          priority
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          className="w-full"
-          src={`/images/advertise/search.png`}
-          alt="adv3"
-          width={500}
-          height={500}
-          priority
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          className="w-full"
-          src={`/images/advertise/rating.png`}
-          alt="adv4"
-          width={500}
-          height={500}
-          priority
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          className="w-full"
-          src={`/images/advertise/pairing.png`}
-          alt="adv5"
-          width={500}
-          height={500}
-          priority
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          className="w-full"
-          src={`/images/advertise/map.png`}
-          alt="adv6"
-          width={500}
-          height={500}
-          priority
-        />
-      </SwiperSlide>
+      {imageList.map((image: string, idx: number) => (
+        <SwiperSlide className="w-full" key={idx}>
+          <Image
+            className="w-full bg-y-gold"
+            src={image}
+            alt="adv1"
+            width={500}
+            height={500}
+            priority
+          />
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 }
