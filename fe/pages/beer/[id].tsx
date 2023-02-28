244c7926503dcd197a0687ddbc768659eb08024a
serin-B <bangserin980302@gmail.com>
2023-02-28 17:06:34 +0900

    fix: else 문으로 setHasRating(false) 추가

--- fe/pages/beer/[id].tsx
+++ fe/pages/beer/[id].tsx
@@ -65,6 +65,8 @@ export default function Beer() {
           if (USERID === response.data.data[0].userId) {
             setHasRating(true);
             setMyRatingId(response.data.data[0].ratingId);
+          } else {
+            setHasRating(false);
           }
         })
         .catch((error) => console.log(error));
