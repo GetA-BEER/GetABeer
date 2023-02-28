8084cde9693aaa932775e0d1ec9c9a35d167953b
Eugenius 1st <eugenius1st@gmail.com>
2023-02-28 13:16:38 +0900

    GB-98 style: speechBalloon 수정

--- fe/components/SpeechBalloon.tsx
+++ fe/components/SpeechBalloon.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { FaPen, FaTrash } from 'react-icons/fa';
+import { HiTrash } from 'react-icons/hi';
+import { MdModeEdit } from 'react-icons/md';
 import axios from '@/pages/api/axios';
 import Image from 'next/image';
 import { TimeHandler } from '@/utils/TimeHandler';
 import CommentInput from './inputs/CommentInput';
 import Swal from 'sweetalert2';
+import ProfileCard from './pairing/ProfileCard';
 
 export interface RatingComment {
   ratingId: number;
@@ -83,50 +85,43 @@ export default function SpeechBalloon({
 
   return (
     <div className="mx-5 mb-4">
-      <div className="w-full h-fit relative ml-4 p-4 rounded-r-lg rounded-b-lg bg-y-cream after:border-t-[30px] after:border-l-[33px] after:border-t-y-cream after:border-l-transparent after:absolute after:top-0 after:-left-8">
-        <div className="flex">
-          <div className="relative rounded-full w-10 h-10 ml-1">
-            <Image
-              alt="user profile image"
-              src={props.userImage}
-              fill
-              className="object-cover"
-            />
-          </div>
-          <div className="flex flex-col ml-2">
-            <span>{props.nickname}</span>
-            <span className="text-xs text-y-gray">{date}</span>
-          </div>
+      <div className="w-full h-fit relative ml-4 p-1 rounded-r-lg rounded-b-lg bg-y-cream after:border-t-[30px] after:border-l-[33px] after:border-t-y-cream after:border-l-transparent after:absolute after:top-0 after:-left-8">
+        <div className="flex items-center">
+          <ProfileCard
+            nickname={props?.nickname}
+            date={date}
+            userImage={props?.userImage}
+          />
+          {isMine ? (
+            <div className="flex-1 flex justify-end items-center  text-y-brown mr-3">
+              <button className="flex items-center mr-1" onClick={editComment}>
+                <MdModeEdit />
+                <span className="text-y-black ml-0.5">수정</span>
+              </button>
+              <button
+                className="flex items-center"
+                onClick={() => {
+                  Swal.fire({
+                    title: '게시글을 삭제하시겠습니까?',
+                    text: '삭제하시면 다시 복구시킬 수 없습니다.',
+                    showCancelButton: true,
+                    confirmButtonColor: '#f1b31c',
+                    cancelButtonColor: '#A7A7A7',
+                    confirmButtonText: '삭제',
+                    cancelButtonText: '취소',
+                  }).then((result) => {
+                    if (result.isConfirmed) {
+                      deleteComment();
+                    }
+                  });
+                }}
+              >
+                <HiTrash className="text-y-brown ml-0.5 inline" />
+                <span className="text-y-black">삭제</span>
+              </button>
+            </div>
+          ) : null}{' '}
         </div>
-        {isMine ? (
-          <div className="flex-1 flex justify-end items-center text-sm text-y-brown mr-2">
-            <button className="flex items-center mr-2" onClick={editComment}>
-              <FaPen />
-              <span className="text-y-black">수정</span>
-            </button>
-            <button
-              className="flex items-center"
-              onClick={() => {
-                Swal.fire({
-                  title: '게시글을 삭제하시겠습니까?',
-                  text: '삭제하시면 다시 복구시킬 수 없습니다.',
-                  showCancelButton: true,
-                  confirmButtonColor: '#f1b31c',
-                  cancelButtonColor: '#A7A7A7',
-                  confirmButtonText: '삭제',
-                  cancelButtonText: '취소',
-                }).then((result) => {
-                  if (result.isConfirmed) {
-                    deleteComment();
-                  }
-                });
-              }}
-            >
-              <FaTrash />
-              <span className="text-y-black">삭제</span>
-            </button>
-          </div>
-        ) : null}
         {isEditMode ? (
           <CommentInput
             inputState={content}
@@ -134,7 +129,9 @@ export default function SpeechBalloon({
             postFunc={patchComment}
           ></CommentInput>
         ) : (
-          <div className="m-3 mt-5 text-sm font-light leading-6">{content}</div>
+          <div className="my-3 mx-5 mt-1 text-xs sm:text-sm font-light leading-6">
+            {content}
+          </div>
         )}
       </div>
     </div>
