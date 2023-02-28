99d979d9f48f6c37d1d4d1ef44154aafb9aadfc4
Bhinney <snakellie97@gmail.com>
2023-02-28 22:47:49 +0900

    GB-60 fix: home 주소

--- be/src/main/java/be/global/home/HomeController.java
+++ be/src/main/java/be/global/home/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-	@GetMapping("/home")
+	@GetMapping("/")
 	public ResponseEntity<String> home() {
 
 		return ResponseEntity.ok("******* 서버 실행 중 *******");
