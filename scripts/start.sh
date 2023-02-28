e8df6b82c43c0725ac54fc2850f9542a9fd8743b
Bhinney <107696895+Bhinney@users.noreply.github.com>
2023-02-28 11:42:49 +0900

    GB-96 Deploy: Update shell

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,8 +6,6 @@ echo "> 현재 시간: $(date)" >> /home/ubuntu/action/deploy.log
 echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploy.log
 
 echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
-DEPLOY_PATH = /home/ubuntu/action/
-cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
@@ -22,6 +20,6 @@ else
 fi
 
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+DEPLOY_JAR = /home/ubuntu/action/be/build/libs/getABeer-0.0.1-SNAPSHOT.jar
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
 sudo nohup java -jar $DEPLOY_JAR --spring.profiles.active=dev >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
