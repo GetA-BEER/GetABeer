f546cb16c9333bcd1c61c3df75b688030eb78277
hyojin <hyoreal51@gmail.com>
2023-03-03 11:43:41 +0900

    GB-83 fix: 핸들러 주석처리

--- be/src/main/java/be/global/security/auth/oauth/handler/OAuth2FailureHandler.java
+++ be/src/main/java/be/global/security/auth/oauth/handler/OAuth2FailureHandler.java
@@ -1,25 +1,25 @@
-package be.global.security.auth.oauth.handler;
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.stereotype.Component;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@Component
-public class OAuth2FailureHandler implements AuthenticationFailureHandler {
-	@Override
-	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-		AuthenticationException exception) throws IOException, ServletException {
-		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-		response.getWriter().write("소셜 로그인에 실패하셨습니다.");
-		log.info("# 소셜로그인 실패 : {}", exception.getMessage());
-	}
-}
+// package be.global.security.auth.oauth.handler;
+//
+// import java.io.IOException;
+//
+// import javax.servlet.ServletException;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
+//
+// import org.springframework.security.core.AuthenticationException;
+// import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+// import org.springframework.stereotype.Component;
+//
+// import lombok.extern.slf4j.Slf4j;
+//
+// @Slf4j
+// @Component
+// public class OAuth2FailureHandler implements AuthenticationFailureHandler {
+// 	@Override
+// 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+// 		AuthenticationException exception) throws IOException, ServletException {
+// 		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+// 		response.getWriter().write("소셜 로그인에 실패하셨습니다.");
+// 		log.info("# 소셜로그인 실패 : {}", exception.getMessage());
+// 	}
+// }
--- be/src/main/java/be/global/security/auth/oauth/service/CustomOAuth2UserService.java
+++ be/src/main/java/be/global/security/auth/oauth/service/CustomOAuth2UserService.java
@@ -175,39 +175,4 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
 		return userRepository.save(createdUser);
 	}
-
-	// loadUser -> 인증코드로 사용자 정보 받아오는 과정
-	// @Override
-	// public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-	// 	log.info("# 소셜로그인 요청 드가자~");
-	//
-	// 	/**
-	// 	 * DefaultOAuth2UserService 객체 생성 -> loadUser(userRequest) -> DefaultOAuth2User 객체를 생성 후 반환
-	// 	 * loadUser() : 소셜 로그인 사용자 정보 제공 URI로 요청 -> 사용자 정보 get -> DefaultOAuth2User 생성 후 반환
-	// 	 */
-	// 	OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
-	// 	OAuth2User oAuth2User = delegate.loadUser(userRequest);
-	//
-	// 	/**
-	// 	 * userRequest 추출한 registrationId 로 provider type 저장
-	// 	 */
-	// 	String registrationId = userRequest.getClientRegistration().getRegistrationId();
-	// 	ProviderType providerType = getProviderType(registrationId);
-	// 	String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint()
-	// 		.getUserNameAttributeName();
-	// 	Map<String, Object> attributes = oAuth2User.getAttributes(); // 소셜 로그인 API 제공 userInfo
-	//
-	// 	OAuth2Attribute extractAttribute = OAuth2Attribute.of(providerType, userNameAttributeName, attributes);
-	//
-	// 	User createdUser = getUser(extractAttribute, providerType);
-	//
-	// 	// DefaultOAuth2User 구현한 CustomOAuth2User 객체 생성 후 반환
-	// 	return new CustomOAuth2User(
-	// 		Collections.singleton(new SimpleGrantedAuthority(createdUser.getRoles().get(0))),
-	// 		attributes,
-	// 		extractAttribute.getUsernameAttributeName(),
-	// 		createdUser.getEmail(),
-	// 		createdUser.getRoles().get(0)
-	// 	);
-	// }
 }
