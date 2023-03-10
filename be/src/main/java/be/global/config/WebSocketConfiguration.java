7e00fa4eb0f017e032872b3045150a051f6bf991
Bhinney <snakellie97@gmail.com>
2023-03-10 22:40:57 +0900

    GB-84 refactor : 로그 삭제

--- be/src/main/java/be/global/config/WebSocketConfiguration.java
+++ be/src/main/java/be/global/config/WebSocketConfiguration.java
@@ -6,37 +6,18 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		log.info("**여기는 웹소켓 컨피그");
-		log.error("**여기는 웹소켓 컨피그");
-
 		registry.addEndpoint("/ws")
 			.setAllowedOriginPatterns("*");
 			// .withSockJS(); /* 코어스 방지 */
-
-		log.info("여기는 웹소켓 컨피그");
-		log.error("여기는 웹소켓 컨피그");
-		log.info("엔드포인트 확인");
-		log.error("엔드포인트 확인");
-		log.info("웹소케케케케켓!!!!!!!!!!");
-		log.error("웹소케케케케켓!!!!!!!!!!");
 	}
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		log.info("여기는 웹소켓 컨피그");
-		log.error("여기는 웹소켓 컨피그");
-		log.info("메세지 브로커");
-		log.error("메세지 브로커");
-		log.info("웹소케케케케켓!!!!!!!!!!");
-		log.error("웹소케케케케켓!!!!!!!!!!");
 		registry.setApplicationDestinationPrefixes("/pub");
 		registry.enableSimpleBroker("/sub");
 	}
