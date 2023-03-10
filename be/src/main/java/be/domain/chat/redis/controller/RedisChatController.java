ff510d082bcce028d524d1ef3525f1cc8001c806
Bhinney <snakellie97@gmail.com>
2023-03-10 22:44:47 +0900

    GB-84 fix: 로그 메세지 수정

--- be/src/main/java/be/domain/chat/redis/controller/RedisChatController.java
+++ be/src/main/java/be/domain/chat/redis/controller/RedisChatController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import be.domain.chat.redis.dto.RedisRoomDto;
@@ -29,7 +28,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-// @RequestMapping("/api/chats")
 @RequiredArgsConstructor
 public class RedisChatController {
 
