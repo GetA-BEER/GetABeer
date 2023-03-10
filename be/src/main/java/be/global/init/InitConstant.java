c6cc68b2cd29f691568e29d91b56409677162a14
Bhinney <snakellie97@gmail.com>
2023-03-10 19:46:26 +0900

    GB-109 fix : deploy시 데이터베이스 초기화 안되도록 수정

--- be/src/main/java/be/global/init/InitConstant.java
+++ be/src/main/java/be/global/init/InitConstant.java
@@ -1,14 +1,14 @@
-package be.global.init;
-
-import be.domain.beer.entity.BeerDetailsCounts;
-
-public class InitConstant {
-	public static final BeerDetailsCounts BEER_DETAILS_COUNTS =
-		BeerDetailsCounts.builder()
-			// .totalStarCount(50)
-			.femaleStarCount(30)
-			.maleStarCount(20)
-			.ratingCount(15)
-			.pairingCount(10)
-			.build();
-}
+// package be.global.init;
+//
+// import be.domain.beer.entity.BeerDetailsCounts;
+//
+// public class InitConstant {
+// 	public static final BeerDetailsCounts BEER_DETAILS_COUNTS =
+// 		BeerDetailsCounts.builder()
+// 			// .totalStarCount(50)
+// 			.femaleStarCount(30)
+// 			.maleStarCount(20)
+// 			.ratingCount(15)
+// 			.pairingCount(10)
+// 			.build();
+// }
