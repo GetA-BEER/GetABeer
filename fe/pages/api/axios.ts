c6b2967eab1efd0a282eea194e693944de6edcfe
Eugenius 1st <eugenius1st@gmail.com>
2023-03-11 00:05:01 +0900

    GB-98 fix-conflict 해결

--- fe/pages/api/axios.ts
+++ fe/pages/api/axios.ts
@@ -8,10 +8,6 @@ let instance = axios.create({
   withCredentials: true,
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> a18285c5d47bbe8aaf902edbbb9ab5b1fbaebc76
 // if (typeof window !== 'undefined') {
 //   // Perform localStorage action
 //   let tmpTOKEN: any = localStorage.getItem('recoil-persist');
@@ -28,10 +24,5 @@ let instance = axios.create({
 //     });
 //   }
 // }
-<<<<<<< HEAD
-=======
->>>>>>> 210f325c2c5ab385cb9061094de8a7ce5924a266
-=======
 
->>>>>>> a18285c5d47bbe8aaf902edbbb9ab5b1fbaebc76
 export default instance;
