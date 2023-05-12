6a85cde6153f0cc077479493c9986d55f8c375d4
Bhinney <snakellie97@gmail.com>
2023-05-12 20:12:52 +0900

    GB-113 refactor : 평가 조회 상태패턴 적용

--- be/src/main/java/be/domain/rating/service/helper/UserNotNull.java
+++ be/src/main/java/be/domain/rating/service/helper/UserNotNull.java
@@ -1,4 +1,29 @@
 package be.domain.rating.service.helper;
 
-public class UserNotNull {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import be.domain.like.repository.RatingLikeRepository;
+import be.domain.rating.dto.RatingResponseDto;
+import be.domain.rating.repository.RatingRepository;
+import be.domain.user.entity.User;
+
+public class UserNotNull implements RatingState{
+	@Override
+	public Page<RatingResponseDto.Total> getRatingResponse(User user, String type, Long beerId, Pageable pageable,
+		RatingRepository ratingRepository, RatingLikeRepository ratingLikeRepository) {
+		Page<RatingResponseDto.Total> responses =
+			ratingRepository.findRatingTotalResponseUserNotNull(beerId, user.getId(), pageable, type);
+		responses.forEach(rating ->
+			rating.addUserLike(getIsUserLikes(rating.getRatingId(), user.getId(), ratingLikeRepository)));
+
+		return responses;
+	}
+
+	/* 추천 여부 가져오기 */
+	private Boolean getIsUserLikes(Long ratingId, Long userId, RatingLikeRepository ratingLikeRepository) {
+		int userLikes = ratingLikeRepository.findRatingLikeUser(ratingId, userId);
+
+		return userLikes != 0;
+	}
 }
